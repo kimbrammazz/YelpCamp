324b86b17efc9ea45f529f0a63f312c340809239
Kim Bramwell Mazzarella <kimbrammazz@gmail.com>
2023-09-14 10:48:27 +1000

    seed database

---
+++ seeds/index.js
@@ -0,0 +1,35 @@
+// file that will run on it's own (seperately from our node app) everytime we want to seed our database
+
+const mongoose = require("mongoose");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
+const Campground = require("../models/campground");
+
+mongoose.connect("mongodb://localhost:27017/yelp-camp");
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+	console.log("database connected");
+});
+
+// array[Math.floor(Math.random() * array.length)]
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+	// first clear database
+	await Campground.deleteMany({});
+	// then seed database
+	for (let i = 0; i, 50; i++) {
+		const random1000 = Math.floor(Math.random() * 1000);
+		const camp = new Campground({
+			location: `${cities[random1000].city}, ${cities[random1000].state}`,
+			title: `${sample(descriptors)} ${sample(places)}`,
+		});
+		await camp.save();
+	}
+};
+
+seedDB().then(() => {
+	mongoose.connection.close();
+});
